14b70c0130029cced1303ad6e98adc94cc722c08
Ivan Frolov <frolik@froliks-MacBook-Pro.local>
2022-07-01 19:22:50 -0400

    changed boto3 lib to aiobotocore

--- s3_fetchers.py
+++ s3_fetchers.py
@@ -1,45 +1,60 @@
+from aiobotocore.session import get_session
+import asyncio
+
 import json
 from typing import List
-import boto3
-import asyncio
 
 import near_types
 
 ESTIMATED_SHARDS_COUNT = 4
 
+AWS_ACCESS_KEY_ID = "xxx"
+AWS_SECRET_ACCESS_KEY = "xxx"
+REGION_NAME = "xxx"
 
-async def list_blocks(s3_client, s3_bucket_name: str, start_from_block_height: near_types.BlockHeight, number_of_blocks_requested: int) -> List[near_types.BlockHeight]:
-    response = s3_client.list_objects_v2(
-        Bucket=s3_bucket_name,
-        Delimiter="/",
-        MaxKeys=number_of_blocks_requested * (1 + ESTIMATED_SHARDS_COUNT),
-        StartAfter="{:012d}".format(start_from_block_height),
-        RequestPayer="requester",
-    )
 
-    block_heights = []
+async def list_blocks(s3_bucket_name: str, start_from_block_height: near_types.BlockHeight, number_of_blocks_requested: int) -> List[near_types.BlockHeight]:
+    session = get_session()
+    async with session.create_client("s3", region_name=REGION_NAME,
+                                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                                     aws_access_key_id=AWS_ACCESS_KEY_ID) as s3_client:
 
-    for prefix in response.get('CommonPrefixes'):
-        block_heights.append(near_types.BlockHeight(
-            int(prefix.get('Prefix')[:-1])))
+        paginator = s3_client.get_paginator("list_objects_v2")
+        async for response in paginator.paginate(
+            Bucket=s3_bucket_name,
+            Delimiter="/",
+            MaxKeys=number_of_blocks_requested * (1 + ESTIMATED_SHARDS_COUNT),
+            StartAfter="{:012d}".format(start_from_block_height),
+            RequestPayer="requester",
+        ):
+            block_heights = []
+
+            for prefix in response.get("CommonPrefixes"):
+                block_heights.append(near_types.BlockHeight(
+                    int(prefix.get("Prefix")[:-1])))
 
     return block_heights
 
 
-async def fetch_streamer_message(s3_client, s3_bucket_name: str, block_height: near_types.BlockHeight) -> near_types.StreamerMessage:
-    response = s3_client.get_object(
-        Bucket=s3_bucket_name,
-        Key="{:012d}/block.json".format(block_height),
-        RequestPayer="requester",
-    )
+async def fetch_streamer_message(s3_bucket_name: str, block_height: near_types.BlockHeight) -> near_types.StreamerMessage:
+    session = get_session()
+    async with session.create_client("s3", region_name=REGION_NAME,
+                                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                                     aws_access_key_id=AWS_ACCESS_KEY_ID) as s3_client:
+        response = await s3_client.get_object(
+            Bucket=s3_bucket_name,
+            Key="{:012d}/block.json".format(block_height),
+            RequestPayer="requester"
+        )
 
-    json_content = json.loads(response['Body'].read().decode('utf-8'))
+        async with response["Body"] as stream:
+            body = await stream.read()
 
+    json_content = json.loads(body.decode("utf-8"))
     block_view = near_types.BlockView.from_dict(json_content)
-    # shards = await asyncio.gather(*[fetch_shard_or_retry(s3_client, s3_bucket_name, block_height, shard_id) for shard_id in range(len(block_view.chunks))])
 
     shards_fetching = [
-        fetch_shard_or_retry(s3_client, s3_bucket_name, block_height, shard_id)
+        fetch_shard_or_retry(s3_bucket_name, block_height, shard_id)
         for shard_id in range(len(block_view.chunks))
     ]
     shards = await asyncio.gather(*shards_fetching)
@@ -47,21 +62,27 @@ async def fetch_streamer_message(s3_client, s3_bucket_name: str, block_height: n
     return near_types.StreamerMessage(block_view, shards)
 
 
-async def fetch_shard_or_retry(s3_client, s3_bucket_name: str, block_height: near_types.BlockHeight, shard_id: int) -> near_types.IndexerShard:
-    response = s3_client.get_object(
-        Bucket=s3_bucket_name,
-        Key="{:012d}/shard_{}.json".format(block_height, shard_id),
-        RequestPayer="requester",
-    )
+async def fetch_shard_or_retry(s3_bucket_name: str, block_height: near_types.BlockHeight, shard_id: int) -> near_types.IndexerShard:
+    session = get_session()
+    async with session.create_client("s3", region_name=REGION_NAME,
+                                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                                     aws_access_key_id=AWS_ACCESS_KEY_ID) as s3_client:
+        response = await s3_client.get_object(
+            Bucket=s3_bucket_name,
+            Key="{:012d}/shard_{}.json".format(block_height, shard_id),
+            RequestPayer="requester"
+        )
 
-    json_content = json.loads(response['Body'].read().decode('utf-8'))
+        async with response["Body"] as stream:
+            body = await stream.read()
+
+    json_content = json.loads(body.decode("utf-8"))
     return near_types.IndexerShard.from_dict(json_content)
 
 
-async def main():
-    s3_client = boto3.client("s3")
-    # print(await list_blocks(s3_client, "near-lake-data-mainnet", 68914412, 100))
-    print(await fetch_streamer_message(s3_client, "near-lake-data-mainnet", 68914412))
-    # print(await fetch_shard_or_retry(s3_client, "near-lake-data-mainnet", 68914412, 3))
+# async def main():
+#     print(await list_blocks("near-lake-data-mainnet", 68914412, 10))
+#     print(await fetch_streamer_message("near-lake-data-mainnet", 68914412))
+#     print(await fetch_shard_or_retry("near-lake-data-mainnet", 68914412, 3))
 
-asyncio.run(main())
+# asyncio.run(main())
