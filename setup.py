8f85fadc09b4eb5862c9d1297cece16e650be41d
Ivan Frolov <frolik@froliks-MacBook-Pro.local>
2022-07-06 20:37:38 -0400

    feat: files to upload near_lake_framework to pypi

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="near-lake-framework",
+    version="0.0.1",
+    author="Ivan Frolov",
+    author_email="frolvanya@gmail.com",
+    description="Python Library to connect to the NEAR Lake S3 and stream the data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/frolvanya/near-lake-framework-py",
+    project_urls={
+        "Bug Tracker": "https://github.com/frolvanya/near-lake-framework-py/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "asyncio",
+        "dataclasses",
+        "dataclasses_json",
+        "aiobotocore"
+    ]
+)
