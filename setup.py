c7fbb9a7cacce8e17974648ad09179cb5b01aaa8
Ivan Frolov <frolvanya@gmail.com>
2024-05-29 10:32:31 -0400

    chore: updated to 0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="near-lake-framework",
-    version="0.0.8",
+    version="0.0.9",
     author="Ivan Frolov",
     author_email="frolvanya@gmail.com",
     description="Python Library to connect to the NEAR Lake S3 and stream the data",
@@ -27,9 +27,13 @@ setuptools.setup(
     install_requires=[
         "asyncio>=3.4.3",
         "dataclasses>=0.6",
-        "dataclasses-json>=0.5.7",
-        "aiobotocore>=2.3.0",
+        "dataclasses-json>=0.6.6",
+        "botocore>=1.34.70",
+        "aiobotocore>=2.13.0",
         "requests>=2.32.2",
+        "types-botocore>=1.0.2",
+        "types-aiobotocore>=2.13.0",
+        "types-requests>=2.32.0.20240523",
     ],
     package_data={"near_lake_framework": ["py.typed"]},
 )
